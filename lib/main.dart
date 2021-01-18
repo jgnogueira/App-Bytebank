22c4c6d94843faa00cceb0375470fdbccc37578e
jgnogueira <josegmnogueira@hotmail.com>
2021-01-18 20:30:42 -0300

    Incluindo Widget MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      Column(
-        children: <Widget>[
-          Text('Deliver features faster', textDirection: TextDirection.ltr),
-          Text('Craft beautiful UIs', textDirection: TextDirection.ltr),
-          Expanded(
-            child: FittedBox(
-              fit: BoxFit.contain, // otherwise the logo will be tiny
-              child: const FlutterLogo(),
-            ),
-          ),
-        ],
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Transferências'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () => null,
+        ),
       ),
-    );
+    ));
