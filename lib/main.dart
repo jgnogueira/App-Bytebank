af8cc0420a8ab24a3587b5623d68c3374effa326
jgnogueira <josegmnogueira@hotmail.com>
2021-01-18 21:24:58 -0300

    Adicionando FloatingActionButton

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,24 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
+        body: Column(
+          children: [
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('100.0'),
+                subtitle: Text('112233'),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('100.0'),
+                subtitle: Text('112233'),
+              ),
+            ),
+          ],
+        ),
         appBar: AppBar(
           title: Text('Transferências'),
         ),
