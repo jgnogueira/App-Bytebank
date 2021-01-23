d62fc7d58e612a436c1b39bb0d8100cfd47b59cc
jgnogueira <josegmnogueira@hotmail.com>
2021-01-23 10:22:06 -0300

    Adicionando parâmetros opcionais no construtor da classe Editor

--- lib/main.dart
+++ lib/main.dart
@@ -25,9 +25,16 @@ class FormularioTransferencia extends StatelessWidget {
         body: Column(
           children: [
             Editor(
-                _controladorCampoNumeroConta, 'Número da Conta', '0000', null),
+              controlador: _controladorCampoNumeroConta,
+              rotulo: 'Número da Conta',
+              dica: '0000',
+            ),
             Editor(
-                _controladorCampoValor, 'Valor', '0.00', Icons.monetization_on),
+              controlador: _controladorCampoValor,
+              rotulo: 'Valor',
+              dica: '0.00',
+              icone: Icons.monetization_on,
+            ),
             RaisedButton(
               onPressed: () {
                 final int numeroConta =
@@ -48,24 +55,24 @@ class FormularioTransferencia extends StatelessWidget {
 }
 
 class Editor extends StatelessWidget {
-  final TextEditingController _controlador;
-  final String _rotulo;
-  final String _dica;
-  final IconData _icone;
+  final TextEditingController controlador;
+  final String rotulo;
+  final String dica;
+  final IconData icone;
 
-  Editor(this._controlador, this._rotulo, this._dica, this._icone);
+  Editor({this.controlador, this.rotulo, this.dica, this.icone});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: TextField(
-        controller: _controlador,
+        controller: controlador,
         style: TextStyle(fontSize: 24.0),
         decoration: InputDecoration(
-          icon: Icon(_icone),
-          labelText: _rotulo,
-          hintText: _dica,
+          icon: icone != null ? Icon(icone) : null,
+          labelText: rotulo,
+          hintText: dica,
         ),
         keyboardType: TextInputType.number,
       ),
